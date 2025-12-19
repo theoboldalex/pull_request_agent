8f83358f60f50edc30e0833425b541554958b7ef
Alex Theobold <alex.theobold@sykescottages.co.uk>
2025-12-19 13:33:55 +0000

    get the diff between the current and main or if on main, just the diff from last commit

--- main.go
+++ main.go
@@ -1,18 +1,45 @@
 package main
 
 import (
-	"context"
+	// "context"
 	"fmt"
-
-	"github.com/nlpodyssey/openai-agents-go/agents"
+	"os/exec"
+	"strings"
+	// "github.com/nlpodyssey/openai-agents-go/agents"
 )
 
 func GetCodeDiff() (string, error) {
+	mainBranch, err := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").Output()
+	if err != nil {
+		return "", err
+	}
+	currentBranch, err := exec.Command("git", "branch", "--show-current").Output()
+	if err != nil {
+		return "", err
+	}
 
+	diffCommand := []string{"diff"}
+	currentBranchName := strings.TrimSpace(string(currentBranch))
+	mainBranchName := strings.TrimSpace(string(mainBranch))
+	if currentBranchName != mainBranchName {
+		diffCommand = append(diffCommand, mainBranchName)
+	}
+
+	diffString, err := exec.Command("git", diffCommand...).Output()
+	if err != nil {
+		return "", err
+	}
+	return string(diffString), nil
 }
 
 func main() {
-	agent := agents.New("Pull Request Agent").
+	diff, err := GetCodeDiff()
+	if err != nil {
+		fmt.Println("Error getting code diff:", err)
+		return
+	}
+	fmt.Println("Code Diff:\n\n", diff)
+	/* agent := agents.New("Pull Request Agent").
 		WithInstructions("You are a helpful assistant that creates pull requests.").
 		WithModel("gpt-4o")
 
@@ -20,10 +47,11 @@ func main() {
 	if err != nil {
 		fmt.Println("Error getting code diff:", err)
 	}
+
 	result, err := agents.Run(context.Background(), agent, diff)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(result.FinalOutput)
+	fmt.Println(result.FinalOutput) */
 }
